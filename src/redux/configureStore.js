1304c897abb9c9c88fe61cfe8eb88d01db7acbaa
tkz96 <tkz9696@gmail.com>
2022-12-07 13:53:18 +0500

    Add thunk to redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({ /* add reducers here */ });
 
 const store = configureStore({
-  reducer: {
-  },
+  middleware: [thunk],
+  reducer: rootReducer,
 });
 
 export default store;
