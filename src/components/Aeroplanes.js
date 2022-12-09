3aeebe0dd9bec27480501dde8ed288b895ba35c5
sharon-odhiambo <sharon.odhiambo100@gmail.com>
2022-12-09 17:49:37 +0300

    update aeroplanes carousels

--- src/components/Aeroplanes.js
+++ src/components/Aeroplanes.js
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux/es/exports';
 import { NavLink } from 'react-router-dom';
 import { FcRight } from 'react-icons/fc';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import { fetchAeroplane } from '../redux/aeroplanes/aeroplanes';
 import '../styles/Homepage.css';
 
@@ -20,23 +22,33 @@ const Aeroplanes = () => {
     const aero = aeroplanes.find((a) => a.id === e.target.id);
     localStorage.setItem('aero', JSON.stringify(aero));
   };
+
+  const handleDragStart = (e) => e.preventDefault();
+  const responsive = {
+    0: { items: 2 },
+    1024: { items: 4 },
+  };
   return (
     <div className="d-flex flex-column justify-content-center align-items-center home">
       <h2>Latest Models</h2>
       <p>Kindly select a plane for renting</p>
-      {aeroplanes.map((a) => (
-        <div key="aero" className="aero">
-          <img src={a.image} alt={a.name} width="250" height="150" />
-          <p>{a.name}</p>
-          <p>{a.description}</p>
-          <NavLink
-            key={a.links}
-            to="/Aeroplane"
-          >
-            <FcRight id={a.id} onClick={onClickHandler} />
-          </NavLink>
-        </div>
-      ))}
+      <AliceCarousel
+        mouseTracking
+        responsive={responsive}
+        items={aeroplanes.map((a) => (
+          <div key="aero" className="aero">
+            <img src={a.image} alt={a.name} onDragStart={handleDragStart} role="presentation" width="150" height="150" className="image" />
+            <p>{a.name}</p>
+            <p>{a.description}</p>
+            <NavLink
+              key={a.links}
+              to="/Aeroplane"
+            >
+              <FcRight id={a.id} onClick={onClickHandler} />
+            </NavLink>
+          </div>
+        ))}
+      />
     </div>
   );
 };
