e05c685e19cf4f5d30ad68130ec281a410e37dde
tkz96 <tkz9696@gmail.com>
2022-12-12 20:50:09 +0500

    Add table and buttons

--- src/components/Aeroplane.js
+++ src/components/Aeroplane.js
@@ -1,30 +1,61 @@
-import { PropTypes } from 'prop-types';
 import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import { aeroplanes } from './Aeroplanes';
+import { GoChevronRight } from 'react-icons/go';
 
 const Aeroplane = () => {
-    const { aeroplaneId } = useParams();
-	const aeroplanes = useSelector((state) => state);
-    const aeroplane = aeroplanes.findIndex(obj => obj.id == aeroplaneId);
-    console.log(aeroplane);
-    return <div>
-        <h1>Aeroplane</h1>
-        <p>{aeroplaneId}</p>
-        <p>{aeroplanes[aeroplane].name}</p>
-        <p>{aeroplanes[aeroplane].description}</p>
-        <p>{aeroplanes[aeroplane].make_year}</p>
-        <p>{aeroplanes[aeroplane].image}</p>
-        <Link to="/">Back to Aeroplanes</Link>
+  const { aeroplaneId } = useParams();
+  const aeroplanes = useSelector((state) => state);
+  // eslint-disable-next-line eqeqeq
+  const plane = aeroplanes.findIndex((obj) => obj.id == aeroplaneId);
+  return (
+    <div className="plane-details">
+      <div>
+        <img className="plane-details-image center" src={aeroplanes[plane].image} alt={aeroplanes[plane].name} />
+      </div>
+      <div className="plane-right d-flex flex-column center">
+        <h1 className="text-center">{aeroplanes[plane].name}</h1>
 
+        <div className="plane-table">
+          <div className="d-flex justify-content-between">
+            <p>Make Year</p>
+            <p>{aeroplanes[plane].make_year}</p>
+          </div>
+          <div className="d-flex justify-content-between">
+            <p>Country of Origin</p>
+            <p>{aeroplanes[plane].country}</p>
+          </div>
+          <div className="d-flex justify-content-between">
+            <p>Fuel ranking</p>
+            <p>{aeroplanes[plane].fuel}</p>
+          </div>
+          <div className="d-flex justify-content-between">
+            <p>Passengers capacity</p>
+            <p>{aeroplanes[plane].passengers}</p>
+          </div>
+          <div className="d-flex justify-content-between">
+            <p>Crew capacity</p>
+            <p>{aeroplanes[plane].crew}</p>
+          </div>
+        </div>
+
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            color: '#4a4a4a',
+            fontWeight: 'bold',
+            fontSize: '1rem',
+          }}
+        >
+          DISCOVER MORE MODELS
+          <GoChevronRight color="#f8c605" />
+        </Link>
+        <Link to="/">
+          <button type="button">Reserve</button>
+        </Link>
+      </div>
     </div>
-}
-
-
-// Aeroplane.propTypes = {
-//     id: PropTypes.number.isRequired,
-//     name: PropTypes.string.isRequired,
-//     make_year: PropTypes.number.isRequired,
-// };
+  );
+};
 
 export default Aeroplane;
--- src/components/Aeroplanes.js
+++ src/components/Aeroplanes.js
@@ -7,40 +7,39 @@ import { fetchAeroplane } from '../redux/aeroplanes/aeroplanes';
 import '../styles/Homepage.css';
 
 const Aeroplanes = () => {
-	const dispatch = useDispatch();
-	const aeroplanes = useSelector((state) => state);
-	useEffect(() => {
-		if (!aeroplanes.length) {
-			dispatch(fetchAeroplane());
-		}
-	}, [dispatch, aeroplanes]);
+  const dispatch = useDispatch();
+  const aeroplanes = useSelector((state) => state);
+  useEffect(() => {
+    if (!aeroplanes.length) {
+      dispatch(fetchAeroplane());
+    }
+  }, [dispatch, aeroplanes]);
 
-	const onClickHandler = (e) => {
-		localStorage.clear();
-		const aero = aeroplanes.find((a) => a.id === e.target.id);
-		localStorage.setItem('aero', JSON.stringify(aero));
-	};
+  const onClickHandler = (e) => {
+    localStorage.clear();
+    const aero = aeroplanes.find((a) => a.id === e.target.id);
+    localStorage.setItem('aero', JSON.stringify(aero));
+  };
 
-	return (
-		<div className="d-flex flex-column justify-content-center align-items-center home">
-			<h2>Latest Models</h2>
-			<p>Kindly select a plane for renting</p>
-			{aeroplanes.map((a) => (
-				<div key={a.id} className="aero">
-					<img src={a.image} alt={a.name} width="250" height="150" />
-					<p>{a.name}</p>
-					<p>{a.description}</p>
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center home">
+      <h2>Latest Models</h2>
+      <p>Kindly select a plane for renting</p>
+      {aeroplanes.map((a) => (
+        <div key={a.id} className="aero">
+          <img src={a.image} alt={a.name} width="250" height="150" />
+          <p>{a.name}</p>
+          <p>{a.description}</p>
 
-					<Link to={`/Aeroplanes/${a.id}`}
-					>
-						<p>View Details</p>
-						<FcRight id={a.id} onClick={onClickHandler} />
-					</Link>
+          <Link to={`/Aeroplanes/${a.id}`}>
+            <p>View Details</p>
+            <FcRight id={a.id} onClick={onClickHandler} />
+          </Link>
 
-				</div>
-			))}
-		</div>
-	);
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Aeroplanes;
