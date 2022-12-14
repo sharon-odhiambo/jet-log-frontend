55e6afcd0482d762c35b6f14c377c3c7ec96e555
unknown <aquinasmoriasi@gmail.com>
2022-12-14 23:13:32 +0300

    Add reservations elemement

---
+++ src/components/Reservations.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchResevations } from '../redux/reservations/reservations';
+
+const Reservations = () => {
+  const dispatch = useDispatch();
+  const reservations = useSelector((state) => state.reservations);
+
+  useEffect(() => {
+    dispatch(fetchResevations());
+  }, [dispatch]);
+  return (
+    <div className="container p-2 reservations pt-5 mt-3">
+      <h2 className="text-center">My Reservations</h2>
+      <Table striped className="me-5">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Aeroplane</th>
+            <th>Start date</th>
+            <th>End date</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          { reservations.map((res, index) => (
+            <tr key={res.id}>
+              <td>{index + 1}</td>
+              <td>{res.aeroplane_name}</td>
+              <td>{res.start_date}</td>
+              <td>{res.end_date}</td>
+              <td>{res.city}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default Reservations;
