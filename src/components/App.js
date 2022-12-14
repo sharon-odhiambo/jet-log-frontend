828f52a476fbc4c9aac3b06c80073946af9dbb40
unknown <aquinasmoriasi@gmail.com>
2022-12-14 23:11:31 +0300

    Add reservations element to routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Aeroplanes from './Aeroplanes';
 import Aeroplane from './Aeroplane';
 import Navbar from './Navbar/Navbar';
+import Reservations from './Reservations';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Aeroplanes />} />
           <Route path="/Aeroplanes" element={<Aeroplanes />} />
           <Route path="/Aeroplanes/:aeroplaneId" element={<Aeroplane />} />
+          <Route path="/reservations" element={<Reservations />} />
         </Routes>
       </div>
     </BrowserRouter>
