b696378b0596e2de8c12789f42ea47921074bfa1
Berkay Akbas <berkayakbas@gmail.com>
2022-12-14 12:26:10 +0300

    Revert "Merge pull request #13 from tkz96/add-plane-to-api"
    This reverts commit 8ddc485a8976f97674e4b90e88c91fbb6d405c4c, reversing
    changes made to 81803ccd8c2a34fd00935da9c29259988efcdf87.

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Aeroplanes from './Aeroplanes';
 import Aeroplane from './Aeroplane';
 import Navbar from './Navbar/Navbar';
-import InputAeroplane from './Aeroplanes/InputAeroplane';
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route path="/" element={<Aeroplanes />} />
           <Route path="/Aeroplanes" element={<Aeroplanes />} />
           <Route path="/Aeroplanes/:aeroplaneId" element={<Aeroplane />} />
-          <Route path="/add-plane" element={<InputAeroplane />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,10 @@
 /* eslint-disable */
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import aeroplanesReducer from './aeroplanes/aeroplanes';
-import addAeroplaneReducer from './aeroplanes/addAeroplanes';
-
-const rootReducer = combineReducers({
-  aeroplanes: aeroplanesReducer,
-  addAeroplanes: addAeroplaneReducer,
-  /* add reducers here */
-});
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: aeroplanesReducer,
   middleware: [thunk],
 });
 
