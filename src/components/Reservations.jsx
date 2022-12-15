353ad8dac337b1c80d9f8d33ea431d7d508fe3dd
unknown <aquinasmoriasi@gmail.com>
2022-12-15 13:55:08 +0300

    Ask user to sign in to access the reservations and also show text when there are no items

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -6,6 +6,7 @@ import { fetchResevations } from '../redux/reservations/reservations';
 const Reservations = () => {
   const dispatch = useDispatch();
   const reservations = useSelector((state) => state.reservations);
+  const auth = localStorage.getItem('session');
 
   useEffect(() => {
     dispatch(fetchResevations());
@@ -13,29 +14,32 @@ const Reservations = () => {
   return (
     <div className="container p-2 reservations pt-5 mt-3">
       <h2 className="text-center">My Reservations</h2>
-      <Table striped className="me-5">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Aeroplane</th>
-            <th>Start date</th>
-            <th>End date</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-
-          { reservations.map((res, index) => (
-            <tr key={res.id}>
-              <td>{index + 1}</td>
-              <td>{res.aeroplane_name}</td>
-              <td>{res.start_date}</td>
-              <td>{res.end_date}</td>
-              <td>{res.city}</td>
+      {!auth && <p className="text-danger text-center">You need to sign in to continue.</p>}
+      {(!reservations.length && auth) && <p className="text-danger text-center">No reservations yet.</p>}
+      {(auth && reservations.length > 0) && (
+        <Table striped className="me-5">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Aeroplane</th>
+              <th>Start date</th>
+              <th>End date</th>
+              <th>City</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {reservations.map((res, index) => (
+              <tr key={res.id}>
+                <td>{index + 1}</td>
+                <td>{res.aeroplane_name}</td>
+                <td>{res.start_date}</td>
+                <td>{res.end_date}</td>
+                <td>{res.city}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </div>
   );
 };
