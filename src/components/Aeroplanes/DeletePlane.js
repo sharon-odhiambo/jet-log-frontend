b2eb0700078ed69c9852b6990bf393754873d99e
sharon-odhiambo <sharon.odhiambo100@gmail.com>
2022-12-15 16:37:25 +0300

    update README and add conditional rendering

--- src/components/Aeroplanes/DeletePlane.js
+++ src/components/Aeroplanes/DeletePlane.js
@@ -18,66 +18,63 @@ const DeleteAeroplanes = () => {
   };
 
   return (
-    <>
-      {!aeroplanes.length && <h6 className="text-center mt-5">No aeroplanes at the moment</h6>}
-      {aeroplanes.length > 0 && (
-      <div className="d-flex flex-column gap-3 justify-content-center align-items-center home">
-        <h2 className="mt-5 pt-5">Delete a Plane</h2>
-        <Carousel
-          plugins={[
-            'arrows',
-            {
-              resolve: slidesToShowPlugin,
-              options: {
-                numberOfSlides: 3,
+    aeroplanes.length > 0 && (
+    <div className="d-flex flex-column gap-3 justify-content-center align-items-center home">
+      <h2 className="mt-5 pt-5">Delete a Plane</h2>
+      <Carousel
+        plugins={[
+          'arrows',
+          {
+            resolve: slidesToShowPlugin,
+            options: {
+              numberOfSlides: 3,
+            },
+          },
+        ]}
+        breakpoints={{
+          640: {
+            plugins: [
+              'arrows',
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 1,
+                },
               },
-            },
-          ]}
-          breakpoints={{
-            640: {
-              plugins: [
-                'arrows',
-                {
-                  resolve: slidesToShowPlugin,
-                  options: {
-                    numberOfSlides: 1,
-                  },
+            ],
+          },
+          1100: {
+            plugins: [
+              'arrows',
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 2,
                 },
-              ],
-            },
-            1100: {
-              plugins: [
-                'arrows',
-                {
-                  resolve: slidesToShowPlugin,
-                  options: {
-                    numberOfSlides: 2,
-                  },
-                },
-              ],
-            },
-          }}
-          mouseTracking
-          slide={false}
-          fade={false}
-        >
-          {aeroplanes.map((a) => (
-            <div key="aero" className="d-flex flex-column justify-content-center align-items-center gap-3">
-              <div className="back">
-                <div
-                  style={{ backgroundImage: `url(${a.image})` }}
-                  id={a.id}
-                  className="image"
-                />
-              </div>
-              <span className="name pt-3">{a.name}</span>
-              <button type="button" id={a.id} onClick={onClickDelete}><span>Delete</span></button>
+              },
+            ],
+          },
+        }}
+        mouseTracking
+        slide={false}
+        fade={false}
+      >
+        {aeroplanes.map((a) => (
+          <div key="aero" className="d-flex flex-column justify-content-center align-items-center gap-3">
+            <div className="back">
+              <div
+                style={{ backgroundImage: `url(${a.image})` }}
+                id={a.id}
+                className="image"
+              />
             </div>
-          ))}
-        </Carousel>
-      </div>
-      )}
-    </>
+            <span className="name pt-3">{a.name}</span>
+            <button type="button" id={a.id} onClick={onClickDelete}><span>Delete</span></button>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+    )
   );
 };
 
